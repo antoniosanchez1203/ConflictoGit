d0a90f8ef9eb3855a1d5b6cca3c917a6b8d82c21
antoniosanchez1203 <1843308@alu.murciaeduca.es>
2026-06-01 13:12:56 +0200

    nueva funcionalidad

--- Assets/scripts/Conflicto.cs
+++ Assets/scripts/Conflicto.cs
@@ -5,7 +5,7 @@ public class Conflicto : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Correcionn de bug aplicada");
+        Debug.Log("Nueva funcionalidad añadida");
     }
        
 }
