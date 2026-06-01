cc60b07b60e552eef0aed8d821b8294f353bac57
antoniosanchez1203 <1843308@alu.murciaeduca.es>
2026-06-01 12:40:37 +0200

    Original

---
+++ Assets/scripts/Conflicto.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Conflicto : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Correcionn de bug aplicada");
+    }
+       
+}
